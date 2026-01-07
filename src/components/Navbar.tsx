6116926b562ab7d1b84a0484e388081f094cc389
keshab-bhandari13 <bhandarikeshab284@gmail.com>
2026-01-07 14:18:18 +0545

    here

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,6 +15,7 @@ export const Navbar: React.FC = () => {
 
   return (
     <nav className="bg-white shadow-sm border-b border-gray-200">
+      aman
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex">
