534ebbfe5adabccd1761bafcd4eef1a9407a73ed
keshab-bhandari13 <bhandarikeshab284@gmail.com>
2026-01-27 23:30:48 +0545

    chnages of unwanted

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -35,7 +35,7 @@ export const Navbar: React.FC = () => {
                   to="/admin"
                   className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
                 >
-                  Admin aman
+                 
                 </Link>
               )}
             </div>
